3dd934e1081c02c44cbcf942ee567b6bf3f18d09
SUG <61492818+Sugilus@users.noreply.github.com>
2023-01-06 11:56:49 +0900

    d

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SMotor.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class SMotor
 {
@@ -13,14 +14,16 @@ public class SMotor
         private double i_position;
         private double angle;
         private double unit = 0.001; //나중에 수정
+        private Telemetry telemetry;
 
-        public void init(HardwareMap hardwaremap, String name, int dir, double s_position)
+        public void init(HardwareMap hardwaremap, Telemetry telemetry, String name, int dir, double s_position)
         {
                 motor = hardwaremap.get(Servo.class, name);
                 finish = true;
                 this.dir = dir;
                 position = s_position*dir;
                 motor.setPosition(position);
+                this.telemetry = telemetry;
         }
         public void update()
         {
