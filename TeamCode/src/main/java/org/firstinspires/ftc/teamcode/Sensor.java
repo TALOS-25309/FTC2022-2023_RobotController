e2e504365744322e390a23669f4473a285bd4281
SUG <61492818+Sugilus@users.noreply.github.com>
2023-01-06 11:30:24 +0900

    d

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sensor.java
@@ -8,24 +8,33 @@ public class Sensor
         private DigitalChannel sensor;
         public boolean finish;
         private boolean reverse;
+        private boolean active;
 
         public void init(HardwareMap hardwaremap, String name, boolean reverse)
         {
                 sensor = hardwaremap.get(DigitalChannel.class, name);
                 sensor.setMode(DigitalChannel.Mode.INPUT);
                 this.reverse = reverse;
+                active = false;
                 finish = false;
         }
         public void update()
         {
-                if(!reverse && sensor.getState())
+                if(active)
                 {
-                        finish = true;
+                        if(!reverse && sensor.getState())
+                        {
+                                finish = true;
+                        }
+                        else if(reverse && sensor.getState())
+                        {
+                                finish = true;
+                        }
                 }
-                else if(reverse && sensor.getState())
-                {
-                        finish = true;
-                }
+        }
+        public void activate()
+        {
+                active = true
         }
         public boolean finish()
         {
