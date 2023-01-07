859c4be9b9567f92784903cf6cd19a35c21b5ebb
CLiF <clif1593@gmail.com>
2023-01-07 13:16:39 +0900

    Update SMotor.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SMotor.java
@@ -15,42 +15,60 @@ public class SMotor
 
         private Servo motor;
         private int dir; //1 or -1
-        private boolean finish;
-        private double position;
+        private double target;
         private long finish_time = 0;
-        private double i_pos;
+        private long prev_time = 0;
+        private double speed;
         Telemetry telemetry;
 
         public void init(HardwareMap hardwaremap, Telemetry telemetry, String name, Direction direction, double initial_position)
         {
                 motor = hardwaremap.get(Servo.class, name);
-                finish = true;
                 this.dir = direction.get_value();
-                position = initial_position * dir % 1;
-                motor.setPosition(position);
+                motor.setPosition(initial_position * dir % 1);
                 this.telemetry = telemetry;
                 this.finish_time = System.currentTimeMillis();
         }
 
         public void update()
         {
-
+                if(this.target != this.motor.getPosition()){
+                        long delta_t = System.currentTimeMillis() - this.prev_time;
+                        double delta_pos = this.speed * delta_t;
+                        double prev_pos = this.motor.getPosition();
+                        double pos = delta_pos + this.motor.getPosition();
+                        if((prev_pos <= this.target && this.target <= pos) || (prev_pos >= this.target && this.target >= pos)) {
+                                pos = this.target;
+                        }
+                        this.motor.setPosition(pos);
+                }
         }
 
-        public void move(double position, long delay){
-                double pos = this.position + position * this.dir;
+        public void move(double position, long delay)
+        {
+                double pos = this.motor.getPosition() + position * this.dir;
                 if(pos < 0) pos = 0;
                 else if(pos > 1) pos = 1;
                 this.motor.setPosition(pos);
+                this.target = pos;
                 this.finish_time = System.currentTimeMillis() + delay;
         }
 
-        public void move(double position, long delay, double speed){
-
+        public void move(double position, long delay, double speed)
+        {
+                double pos = this.motor.getPosition() + position * this.dir;
+                if(speed < 0) speed *= -1;
+                if(pos < this.motor.getPosition()) speed *= -1;
+                if(pos < 0) pos = 0;
+                else if(pos > 1) pos = 1;
+                this.target = pos;
+                this.finish_time = System.currentTimeMillis() + delay;
+                this.speed = speed;
+                this.prev_time = System.currentTimeMillis();
         }
 
         public boolean finish()
         {
-                return System.currentTimeMillis() >= this.finish_time;
+                return System.currentTimeMillis() >= this.finish_time && this.target == this.motor.getPosition();
         }
 }
