97e335e19107b4b62714022ecc26ed5a4a31ae9a
CLiF <clif1593@gmail.com>
2023-01-07 22:58:52 +0900

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -6,15 +6,18 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Test extends Part
 {
-    private SMotor s1 = new SMotor(), s2 = new SMotor();
+    Sensor touch = new Sensor();
+    Sensor magnet = new Sensor();
+    DMotor motor = new DMotor();
 
     public void init(HardwareMap hwm, Telemetry tel)
     {
-        s1.init(hwm, tel, "servo1", SMotor.Direction.Reverse, 0);
-        s2.init(hwm, tel, "servo2", SMotor.Direction.Direct, 0);
-        DMotor[] dl = {};
-        SMotor[] sl = {s1, s2};
-        Sensor[] snl = {};
+        //touch.init(hwm, tel, "touch", true);
+        motor.init(hwm, tel, "motor", DMotor.Direction.Reverse);
+        magnet.init(hwm, tel, "magnet", true);
+        DMotor[] dl = {motor};
+        SMotor[] sl = {};
+        Sensor[] snl = {magnet};
 
         this.telemetry = tel;
 
@@ -23,22 +26,20 @@ public class Test extends Part
     }
     public void start()
     {
-        start_step("Test");
+        start_step("magnet");
     }
     protected void next_step()
     {
         switch (move_type)
         {
-            case "Test" :
-                switch (step % 3)
+            case "magnet" :
+                switch (step % 2)
                 {
                     case 0:
-                        this.s1.move(1.0, 1, 1.0);
+                        magnet.activate();
                         break;
                     case 1:
-                        break;
-                    case 2:
-                        this.s1.move(-1.0, 1, 0.5);
+                        motor.move(0.5, 0.5);
                         break;
                 }
         }
