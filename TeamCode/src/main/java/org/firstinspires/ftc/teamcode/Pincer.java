5031c760fc77756ec8c119aa682be69bd3135bfe
SUG <61492818+Sugilus@users.noreply.github.com>
2023-01-09 13:41:06 +0900

    x

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pincer.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Pincer extends Part
+{
+
+}
