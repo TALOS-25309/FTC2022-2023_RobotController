ed294e108193bcfde253360dc697b811e01b0ac2
CLiF <clif1593@gmail.com>
2023-01-11 20:22:33 +0900

    Update TeleOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -11,6 +11,7 @@ public class TeleOpMode extends OpMode {
 
     private double slow_rate;
     private boolean pincer_up;
+    private boolean adjusting = false;
 
     @Override
     public void init()
@@ -35,64 +36,66 @@ public class TeleOpMode extends OpMode {
         linear_part.update();
         pincer_part.update();
         //*
-        //Wheel Part
-        if(gamepad1.left_trigger > 0.7 || gamepad1.right_trigger > 0.7)
-        {
-            slow_rate = 0.3;
-        }
-        else
-        {
-            slow_rate = 1.0;
-        }
-
-        if(gamepad1.dpad_up) {
-            wheel_part.move(0.5*slow_rate, Wheel.Direction.Forward);
-        }
-        else if(gamepad1.dpad_down) {
-            wheel_part.move(0.5*slow_rate, Wheel.Direction.Backward);
-        }
-        else if(gamepad1.dpad_right) {
-            wheel_part.move(0.5*slow_rate, Wheel.Direction.Right);
-        }
-        else if(gamepad1.dpad_left) {
-            wheel_part.move(0.5*slow_rate, Wheel.Direction.Left);
-        }
-        else if(gamepad1.left_bumper){
-            wheel_part.move(0.5*slow_rate, Wheel.Direction.TurnLeft);
-        }
-        else if(gamepad1.right_bumper){
-            wheel_part.move(0.5*slow_rate, Wheel.Direction.TurnRight);
-        }
-        else {
-            wheel_part.move(0.0,  Wheel.Direction.Forward);
-        }
-        //*/
-
-        //Linear
-        //*
-        if(linear_part.finish() && pincer_part.finish()) {
-            if (gamepad2.y) {
-                pincer_part.start_step("release");
-                linear_part.start_step("simple_stack_cup");
-            } else if (gamepad2.x) {
-                pincer_part.start_step("release");
-                linear_part.start_step("go_low");
-            } else if (gamepad2.b) {
-                pincer_part.start_step("release");
-                linear_part.start_step("go_high");
+        if(!adjusting) {
+            //Wheel Part
+            if(gamepad1.left_trigger > 0.7 || gamepad1.right_trigger > 0.7)
+            {
+                slow_rate = 0.3;
             }
-        }
-        //*/
+            else
+            {
+                slow_rate = 1.0;
+            }
 
-        if(pincer_part.finish()) {
-            if (gamepad2.a) {
-                pincer_part.start_step("pick_up");
+            if(gamepad1.dpad_up) {
+                wheel_part.move(0.5*slow_rate, Wheel.Direction.Forward);
+            }
+            else if(gamepad1.dpad_down) {
+                wheel_part.move(0.5*slow_rate, Wheel.Direction.Backward);
+            }
+            else if(gamepad1.dpad_right) {
+                wheel_part.move(0.5*slow_rate, Wheel.Direction.Right);
+            }
+            else if(gamepad1.dpad_left) {
+                wheel_part.move(0.5*slow_rate, Wheel.Direction.Left);
+            }
+            else if(gamepad1.left_bumper){
+                wheel_part.move(0.5*slow_rate, Wheel.Direction.TurnLeft);
+            }
+            else if(gamepad1.right_bumper){
+                wheel_part.move(0.5*slow_rate, Wheel.Direction.TurnRight);
+            }
+            else {
+                wheel_part.move(0.0,  Wheel.Direction.Forward);
+            }
+            //*/
+
+            //Linear
+            //*
+            if(linear_part.finish() && pincer_part.finish()) {
+                if (gamepad2.y) {
+                    pincer_part.start_step("release");
+                    linear_part.start_step("simple_stack_cup");
+                } else if (gamepad2.x) {
+                    pincer_part.start_step("release");
+                    linear_part.start_step("go_low");
+                } else if (gamepad2.b) {
+                    pincer_part.start_step("release");
+                    linear_part.start_step("go_high");
+                }
+            }
+            //*/
+
+            if(pincer_part.finish()) {
+                if (gamepad2.a) {
+                    pincer_part.start_step("pick_up");
+                }
             }
         }
 
         //adjust
         if(gamepad2.right_bumper && gamepad2.left_bumper){
-            //*
+            this.adjusting = true;
             if(pincer_part.finish()){
                 if(gamepad2.dpad_up){
                     this.pincer_part.adjust_axis(1);
@@ -104,8 +107,6 @@ public class TeleOpMode extends OpMode {
                     this.pincer_part.adjust_axis(0);
                 }
             }
-            //*/
-            //*
             if(linear_part.finish()){
                 if(gamepad2.right_trigger > 0.1) {
                     this.linear_part.adjust_rope(gamepad2.right_trigger);
@@ -117,7 +118,11 @@ public class TeleOpMode extends OpMode {
                     this.linear_part.adjust_rope(0);
                 }
             }
-            //*/
+        }
+        else if(adjusting){
+            this.pincer_part.adjust_axis(0);
+            this.linear_part.adjust_rope(0);
+            this.adjusting = false;
         }
     }
 }
